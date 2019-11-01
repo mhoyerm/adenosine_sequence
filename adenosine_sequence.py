f5d4099425b0dd1ab23f3ab726478b5c4f685088
mhoyerm <45763026+mhoyerm@users.noreply.github.com>
2019-11-01 10:22:24 -0300

    Add files via upload

---
+++ adenosine_sequence.py
@@ -0,0 +1,98 @@
+import sys
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description='Search for adenosines in a roll.', usage='[options]')
+parser.add_argument('-F', '--fasta', type=str, required=True, help='fasta archive containing proteins list')
+parser.add_argument('-O', '--output', type=str, required=True, help='output file (csv format)')
+parser.add_argument('-N', '--number', type=int, required=True, help='Insert wanted number of adenosines in a roll')
+parser.add_argument('-P', '--finalpos', type=int, default=999999, help='Position of the last nucleotid of each gene to be searched for')
+parser.add_argument('-I', '--initialpos', type=int, default=0, help='Position of the last nucleotid of each gene to be searched for')
+args = parser.parse_args()
+
+def percent(index, genome_length): # Iterface updating the percentage of the ORFs read
+	percentage = 100*index/genome_length
+	interface = int(percentage)
+	print str(interface) + '% complete\r',
+import sys
+
+
+################################################################################################
+
+def evaluate(prote, fout, prot_name):
+	charge = 0
+	sequence = False
+	#for each character (does not include the last field size characteres)
+
+	end_pos = len(prote)
+	if end_pos > args.finalpos:
+		end_pos = args.finalpos
+
+	if args.initialpos > end_pos:
+		return
+
+	for i in range(args.initialpos, end_pos):
+		if prote[i] == 'A':
+			charge += 1
+			if charge == 1:
+				beginning = (i + 1)
+			
+		else:
+			if sequence == True:
+				fout.write(str(charge))
+				sequence = False
+			charge = 0
+
+
+
+		if charge >= args.number:
+			if sequence == False:
+				relative = float(i) / float(len(prote))
+				fout.write("\n" + str(prot_name) + ";" + str(beginning) + ";" + str(relative) + ';')
+				sequence = True
+
+	if sequence == True:
+		fout.write(str(charge))
+
+	return
+
+
+
+###################################################################################
+def main():
+	file_in = open(args.fasta, 'r') # Input file
+	in_file = file_in.read()
+
+	out_file = open(args.output, 'w') # Output file
+	
+	actual_path = os.getcwd() # Get file path
+	
+	header_basic = 'Search for arginines or lysines in a roll.\n'
+	header_fasta = 'fasta file:;' + actual_path + '/' + args.fasta + '\n' # Prepare fasta path
+	header_difference = 'number of R/K wanted:;' + str(args.number) + '\n'
+	header_gene_stretch = 'position of aminoacids in gene:;' + str(args.initialpos) + ' - ' + str(args.finalpos) + '\n\n\n\n\n\n'
+	header_data = 'name;position (in nucleotides);relative position;length'
+	
+	file_header = header_basic + header_fasta + header_difference + header_gene_stretch + header_data # Build header without difference value
+	out_file.write(file_header+'\n') # Write header
+
+	proteome_list = in_file.replace('>UniRef100_','>')
+	proteome_list = str.split(in_file, '>') # Split genes by '>'
+
+	for i in range(len(proteome_list)): # Repeat for each gene
+		percent(i, (len(proteome_list)))
+
+		protein_code = ''
+
+		proteome_list[i] = proteome_list[i].replace('\r', '')
+		protein_by_line = str.split(proteome_list[i], '\n') # Split each line
+		header = protein_by_line[0] # The first line is the header
+		name = str.split(header,' ')[0]
+		for j in range(1, len(protein_by_line)):
+			protein_code += protein_by_line[j] # Every line (except the first) is part of the code
+
+		protein_aminoacids = list(protein_code) # List of all aminoacids in a gene
+		evaluate(protein_aminoacids, out_file, name)
+	print '\r100% complete!'
+
+main()
